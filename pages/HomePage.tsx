0868811ebf17383a553d5e423e4196e4740fc7db
Ali Balouchi <alibalouchi.74@gmail.com>
2021-01-12 15:48:01 +0330

    show box and storage photos in different tabs

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,73 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Image,
-  FlatList,
-  PermissionsAndroid,
-  Platform,
-  Text,
-  Button,
-} from 'react-native';
-import CameraRoll from '@react-native-community/cameraroll';
-import FastImage from 'react-native-fast-image';
+import {View, Tabs, Tab, Container, Content} from 'native-base'
+import StoragePhotos from '../components/StoragePhotos'
+import BoxPhotos from '../components/BoxPhotos'
 
 const HomePage = () => {
-  const [photos, setPhotos] = useState<any>();
-
-  async function getPermission() {
-    if (Platform.OS === 'android') {
-      const result = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-        {
-          title: 'Permission Explanation',
-          message: 'ReactNativeForYou would like to access your photos!',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (result !== 'granted') {
-        console.log('Access to pictures was denied');
-        return;
-      }
-    }
-    CameraRoll.getPhotos({
-      first: 50,
-      assetType: 'Photos',
-    })
-      .then((res) => {
-        setPhotos(res.edges);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  useEffect(() => {
-    getPermission();
-  }, []);
 
   return (
-    <View>
-      <FlatList
-        key="photos-flatList"
-        data={photos}
-        numColumns={3}
-        renderItem={({item}) => (
-          <FastImage
-            key={item.node.image.uri}
-            style={{
-              width: '33%',
-              height: 150,
-            }}
-            source={{uri: item.node.image.uri}}
-            // resizeMode={FastImage.resizeMode.contain}
-          />
-        )}
-      />
-    </View>
+    <Container>
+      <Content>
+      <Tabs>
+        <Tab heading="Storage Photos">
+          <StoragePhotos />
+        </Tab>
+        <Tab heading="The Box Photos">
+          <BoxPhotos />
+        </Tab>
+      </Tabs>
+      </Content>
+    </Container>
   );
 };
 
