48db9206d734f929fc2ea602af3fcea05ceaf1ef
Ali Balouchi <alibalouchi.74@gmail.com>
2021-01-12 15:48:11 +0330

    permission error page

---
+++ pages/PermissionError.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const PermissionError = () => {
+  return (
+    <View>
+      <Text>Permission Error</Text>
+    </View>
+  );
+};
+
+export default PermissionError;
