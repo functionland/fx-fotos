b2cbc62652b786b75f10827e5b5b0f9fd847226e
Ali Balouchi <alibalouchi.74@gmail.com>
2021-03-01 21:27:57 +0330

    - add set pinch or zoom function to props - add panresponder to useRef - change pinch or zoom props when on zoom or pinch

--- components/PinchZoom.tsx
+++ components/PinchZoom.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Animated, Dimensions, PanResponder} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {reduxState, sortCondition} from '../types/interfaces';
@@ -14,6 +14,7 @@ const SCREEN_HEIGHT = Dimensions.get('screen').height;
 
 interface Props {
   distance: Animated.Value;
+  setPinchOrZoom: Function;
 }
 
 const PinchZoom: React.FC<Props> = (props) => {
@@ -67,80 +68,84 @@ const PinchZoom: React.FC<Props> = (props) => {
     }
   };
 
-  const panResponder = useState(() =>
-    PanResponder.create({
-      onStartShouldSetPanResponder: (event, gesture) => {
-        let touches = event.nativeEvent.touches;
-        if (gesture.numberActiveTouches == 2) {
-          zIndex = 2;
-          return true;
-        } else {
-          zIndex = 0;
-          return false;
-        }
-      },
-      onPanResponderStart: (initial_event, initial_gesture) => {
-        let touches = initial_event.nativeEvent.touches;
-
-        initialXs = [];
-        initialYs = [];
-
-        initialXs.push(touches[0].locationX);
-        initialXs.push(touches[1].locationX);
-        initialYs.push(touches[0].locationY);
-        initialYs.push(touches[1].locationY);
-      },
-      onPanResponderMove: (event, gesture) => {
-        let touches = event.nativeEvent.touches;
-
-        if (touches.length == 2) {
-          currentXs = [];
-          currentYs = [];
-
-          currentXs.push(touches[0].locationX);
-          currentXs.push(touches[1].locationX);
-          currentYs.push(touches[0].locationY);
-          currentYs.push(touches[1].locationY);
-
-          initial_distance = getDistance(
-            initialXs[0],
-            initialYs[0],
-            initialXs[1],
-            initialYs[1],
-          );
-          current_distance = getDistance(
-            currentXs[0],
-            currentYs[0],
-            currentXs[1],
-            currentYs[1],
-          );
-        }
-
-        props.distance.setValue(initial_distance - current_distance);
-      },
-      onPanResponderRelease: () => {
+  const panResponder = useRef(PanResponder.create({
+    onStartShouldSetPanResponder: (event, gesture) => {
+      let touches = event.nativeEvent.touches;
+      if (gesture.numberActiveTouches == 2) {
+        zIndex = 2;
+        return true;
+      } else {
         zIndex = 0;
-        let animationProgress = (props.distance as any)._value;
-        if (Math.abs(animationProgress) < SCREEN_WIDTH * 0.1) {
-          Animated.timing(props.distance, {
-            toValue: -SCREEN_WIDTH * 0.8,
-            duration: 250,
-            useNativeDriver: false,
-          }).start();
-        } else if (animationProgress < -(SCREEN_WIDTH * 0.1)) {
-          Animated.spring(props.distance, {
-            toValue: -SCREEN_WIDTH * 0.8,
-            useNativeDriver: false,
-          }).start((event) => animationTransition(event, 'zoom'));
-        } else if (animationProgress > SCREEN_WIDTH * 0.1) {
-          Animated.spring(props.distance, {
-            toValue: SCREEN_WIDTH * 0.8,
-            useNativeDriver: false,
-          }).start((event) => animationTransition(event, 'pinch'));
-        }
-      },
-    }),
-  )[0];
+        return false;
+      }
+    },
+    onPanResponderStart: (initial_event, initial_gesture) => {
+      let touches = initial_event.nativeEvent.touches;
+
+      initialXs = [];
+      initialYs = [];
+
+      initialXs.push(touches[0].locationX);
+      initialXs.push(touches[1].locationX);
+      initialYs.push(touches[0].locationY);
+      initialYs.push(touches[1].locationY);
+    },
+    onPanResponderMove: (event, gesture) => {
+      let touches = event.nativeEvent.touches;
+
+      if (touches.length == 2) {
+        currentXs = [];
+        currentYs = [];
+
+        currentXs.push(touches[0].locationX);
+        currentXs.push(touches[1].locationX);
+        currentYs.push(touches[0].locationY);
+        currentYs.push(touches[1].locationY);
+
+        initial_distance = getDistance(
+          initialXs[0],
+          initialYs[0],
+          initialXs[1],
+          initialYs[1],
+        );
+        current_distance = getDistance(
+          currentXs[0],
+          currentYs[0],
+          currentXs[1],
+          currentYs[1],
+        );
+      }
+
+      props.distance.setValue(initial_distance - current_distance);
+    },
+    onPanResponderRelease: () => {
+      zIndex = 0;
+      let animationProgress = (props.distance as any)._value;
+      if (Math.abs(animationProgress) < SCREEN_WIDTH * 0.1) {
+        Animated.timing(props.distance, {
+          toValue: -SCREEN_WIDTH * 0.8,
+          duration: 250,
+          useNativeDriver: false,
+        }).start();
+      } else if (animationProgress < -(SCREEN_WIDTH * 0.1)) {
+        Animated.spring(props.distance, {
+          toValue: -SCREEN_WIDTH * 0.8,
+          useNativeDriver: false,
+        }).start((event) => {
+          animationTransition(event, 'zoom');
+          props.setPinchOrZoom('zoom');
+        });
+      } else if (animationProgress > SCREEN_WIDTH * 0.1) {
+        Animated.spring(props.distance, {
+          toValue: SCREEN_WIDTH * 0.8,
+          useNativeDriver: false,
+        }).start((event) => {
+          animationTransition(event, 'pinch');
+          props.setPinchOrZoom('pinch');
+        });
+      }
+    },
+  })).current;
 
   return (
     <Animated.View
