eab7ad2dd5b2016ef8bfea06577dcd62986d8803
Ali Balouchi <alibalouchi.74@gmail.com>
2021-03-04 10:10:58 +0330

    remove redux

--- components/PinchZoom.tsx
+++ components/PinchZoom.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Animated, Dimensions, PanResponder} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
 import {reduxState, sortCondition} from '../types/interfaces';
 import {Constant} from '../utils/constants';
 import {
@@ -15,6 +14,10 @@ const SCREEN_HEIGHT = Dimensions.get('screen').height;
 interface Props {
   distance: Animated.Value;
   setPinchOrZoom: Function;
+  setSortCondition: Function;
+  setNumColumns: Function;
+  numColumns: 2 | 3 | 4;
+  sortCondition: sortCondition;
 }
 
 const PinchZoom: React.FC<Props> = (props) => {
@@ -26,126 +29,101 @@ const PinchZoom: React.FC<Props> = (props) => {
   let initial_distance: number = 0;
   let current_distance: number = 0;
 
-  const sortCondition = useSelector((state: reduxState) => state.sortCondition);
-  const numColumns = useSelector((state: reduxState) => state.numColumns);
-
-  const [sortConditionState, setSortConditionState] = useState<string>('day');
-  const [numColumnsState, setNumColumnsState] = useState<number>(2);
-
-  useEffect(() => {
-    // console.log(sortCondition, numColumns)
-    if (sortConditionState) {
-      dispatch({
-        type: Constant.actionTypes.sortConditionChange,
-        payload: sortConditionState,
-      });
-    }
-  }, [sortConditionState]);
-
-  useEffect(() => {
-    if (numColumnsState) {
-      dispatch({
-        type: Constant.actionTypes.numColumnsChange,
-        payload: numColumnsState,
-      });
-    }
-  }, [numColumnsState]);
-
-  const dispatch = useDispatch();
-
   const animationTransition = (
     event: {finished: boolean},
     pinchOrZoom: 'pinch' | 'zoom',
   ) => {
     let _sortCondition = changeSortCondition(
-      sortCondition,
+      props.sortCondition,
       pinchOrZoom,
-      numColumns,
+      props.numColumns,
     );
     if (event.finished) {
-      setSortConditionState(_sortCondition.sortCondition);
-      setNumColumnsState(_sortCondition.numColumns);
+      props.setSortCondition(_sortCondition.sortCondition);
+      props.setNumColumns(_sortCondition.numColumns);
     }
   };
 
-  const panResponder = useRef(PanResponder.create({
-    onStartShouldSetPanResponder: (event, gesture) => {
-      let touches = event.nativeEvent.touches;
-      if (gesture.numberActiveTouches == 2) {
-        zIndex = 2;
-        return true;
-      } else {
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: (event, gesture) => {
+        let touches = event.nativeEvent.touches;
+        if (gesture.numberActiveTouches == 2) {
+          zIndex = 2;
+          return true;
+        } else {
+          zIndex = 0;
+          return false;
+        }
+      },
+      onPanResponderStart: (initial_event, initial_gesture) => {
+        let touches = initial_event.nativeEvent.touches;
+
+        initialXs = [];
+        initialYs = [];
+
+        initialXs.push(touches[0].locationX);
+        initialXs.push(touches[1].locationX);
+        initialYs.push(touches[0].locationY);
+        initialYs.push(touches[1].locationY);
+      },
+      onPanResponderMove: (event, gesture) => {
+        let touches = event.nativeEvent.touches;
+
+        if (touches.length == 2) {
+          currentXs = [];
+          currentYs = [];
+
+          currentXs.push(touches[0].locationX);
+          currentXs.push(touches[1].locationX);
+          currentYs.push(touches[0].locationY);
+          currentYs.push(touches[1].locationY);
+
+          initial_distance = getDistance(
+            initialXs[0],
+            initialYs[0],
+            initialXs[1],
+            initialYs[1],
+          );
+          current_distance = getDistance(
+            currentXs[0],
+            currentYs[0],
+            currentXs[1],
+            currentYs[1],
+          );
+        }
+
+        props.distance.setValue(initial_distance - current_distance);
+      },
+      onPanResponderRelease: () => {
         zIndex = 0;
-        return false;
-      }
-    },
-    onPanResponderStart: (initial_event, initial_gesture) => {
-      let touches = initial_event.nativeEvent.touches;
-
-      initialXs = [];
-      initialYs = [];
-
-      initialXs.push(touches[0].locationX);
-      initialXs.push(touches[1].locationX);
-      initialYs.push(touches[0].locationY);
-      initialYs.push(touches[1].locationY);
-    },
-    onPanResponderMove: (event, gesture) => {
-      let touches = event.nativeEvent.touches;
-
-      if (touches.length == 2) {
-        currentXs = [];
-        currentYs = [];
-
-        currentXs.push(touches[0].locationX);
-        currentXs.push(touches[1].locationX);
-        currentYs.push(touches[0].locationY);
-        currentYs.push(touches[1].locationY);
-
-        initial_distance = getDistance(
-          initialXs[0],
-          initialYs[0],
-          initialXs[1],
-          initialYs[1],
-        );
-        current_distance = getDistance(
-          currentXs[0],
-          currentYs[0],
-          currentXs[1],
-          currentYs[1],
-        );
-      }
-
-      props.distance.setValue(initial_distance - current_distance);
-    },
-    onPanResponderRelease: () => {
-      zIndex = 0;
-      let animationProgress = (props.distance as any)._value;
-      if (Math.abs(animationProgress) < SCREEN_WIDTH * 0.1) {
-        Animated.timing(props.distance, {
-          toValue: -SCREEN_WIDTH * 0.8,
-          duration: 250,
-          useNativeDriver: false,
-        }).start();
-      } else if (animationProgress < -(SCREEN_WIDTH * 0.1)) {
-        Animated.spring(props.distance, {
-          toValue: -SCREEN_WIDTH * 0.8,
-          useNativeDriver: false,
-        }).start((event) => {
-          animationTransition(event, 'zoom');
-          props.setPinchOrZoom('zoom');
-        });
-      } else if (animationProgress > SCREEN_WIDTH * 0.1) {
-        Animated.spring(props.distance, {
-          toValue: SCREEN_WIDTH * 0.8,
-          useNativeDriver: false,
-        }).start((event) => {
-          animationTransition(event, 'pinch');
-          props.setPinchOrZoom('pinch');
-        });
-      }
-    },
-  })).current;
+        let animationProgress = (props.distance as any)._value;
+        if (Math.abs(animationProgress) < SCREEN_WIDTH * 0.1) {
+          Animated.timing(props.distance, {
+            toValue: -SCREEN_WIDTH * 0.8,
+            duration: 250,
+            useNativeDriver: false,
+          }).start();
+        } else if (animationProgress < -(SCREEN_WIDTH * 0.1)) {
+          Animated.spring(props.distance, {
+            toValue: -SCREEN_WIDTH * 0.8,
+            useNativeDriver: false,
+          }).start((event) => {
+            animationTransition(event, 'zoom');
+            props.setPinchOrZoom('zoom');
+          });
+        } else if (animationProgress > SCREEN_WIDTH * 0.1) {
+          Animated.spring(props.distance, {
+            toValue: SCREEN_WIDTH * 0.8,
+            useNativeDriver: false,
+          }).start((event) => {
+            animationTransition(event, 'pinch');
+            props.setPinchOrZoom('pinch');
+          });
+        }
+      },
+    }),
+  ).current;
 
   return (
     <Animated.View
