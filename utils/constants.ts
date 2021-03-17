6647ea83c565efa9052f3aa10911c3ff0817a57a
Ali Balouchi <alibalouchi.74@gmail.com>
2021-03-17 12:37:28 +0330

    add error to redux

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,10 +3,13 @@ export const Constant = {
     user: {
       setUser: 'SET_USER',
     },
+    error: {
+      setError: 'SET_ERROR',
+    },
     photos: {
-      getPhotosRequest: "GET_PHOTOS_REQUEST",
-      getPhotosSuccess: "GET_PHOTOS_SUCCESS",
-      getPhotosFailure: "GET_PHOTOS_FAILURE"
+      getPhotosRequest: 'GET_PHOTOS_REQUEST',
+      getPhotosSuccess: 'GET_PHOTOS_SUCCESS',
+      getPhotosFailure: 'GET_PHOTOS_FAILURE',
     },
     box: {
       setBoxMedia: 'SET_BOX_MEDIA',
