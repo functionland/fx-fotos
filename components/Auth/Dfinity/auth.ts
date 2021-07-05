615f33bb89db5d60c5bae0ab925c798fe7083d6b
ehsan shariati <ehsan6sha@gmail.com>
2021-07-05 12:22:52 -0400

    removed Dfinity libraries
    Removed Dfinity libraries to use direct http APIs

--- components/Auth/Dfinity/auth.ts
+++ components/Auth/Dfinity/auth.ts
@@ -1,12 +1,3 @@
-import 'text-encoding-polyfill';
-import { Actor, HttpAgent, } from "@dfinity/agent";
-import { DelegationIdentity } from "@dfinity/identity";
-import { AuthClient } from "@dfinity/auth-client";
-
 export const login = async() => {
-    const authClient = await AuthClient.create();
-    /*authClient.login({
-        identityProvider: "https://identity.ic0.app/",
-        onSuccess: ()=>{console.log('success');}
-    });*/
+    
 }
