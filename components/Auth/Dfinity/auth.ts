159f1b8e4ee7126b06f30659939e9e0f7739d472
ehsan shariati <ehsan6sha@gmail.com>
2021-07-06 18:25:54 -0400

    Polyfill @dfinity auth
    1-Added
    if (!global.localStorage) {
      global.localStorage = {
        _data       : {},
        setItem     : function(id, val) { return this._data[id] = String(val); },
        getItem     : function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
        removeItem  : function(id) { return delete this._data[id]; },
        clear       : function() { return this._data = {}; }
      };
    }
    to
    C:\Users\ehsan\Github\Photos\node_modules\@dfinity\auth-client\lib\cjs\index.js
    
    2- Added
    if (typeof BigInt === 'undefined') global.BigInt = require('big-integer');
    to
    C:\Users\ehsan\Github\Photos\node_modules\@dfinity\authentication\lib\cjs\index.js

--- components/Auth/Dfinity/auth.ts
+++ components/Auth/Dfinity/auth.ts
@@ -1,9 +1,15 @@
 import 'text-encoding';
-import { HttpAgent, } from "@dfinity/agent";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { Principal } from "@dfinity/principal";
+import { DelegationIdentity } from "@dfinity/identity";
+import { AuthClient } from "@dfinity/auth-client";
 
 export const login = async() => {
-    const http = new HttpAgent({
+    /*const http = new HttpAgent({
         host: "https://identity.ic0.app/",
     });
-    /*http.status().then((res)=>{console.log(res)})*/
+    http.status().then((res)=>{console.log(res)})*/
+    const authClient = await AuthClient.create();
+    const identity = authClient.getIdentity();
+    console.log(identity.getPrincipal());
 }
