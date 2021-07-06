6c4be8625f0c98e5953cd6c87eb1c4a5569fd306
ehsan shariati <ehsan6sha@gmail.com>
2021-07-06 17:44:56 -0400

    Resolved BigInt not found in Dfinity
    Added
    if (typeof BigInt === 'undefined') global.BigInt = require('big-integer')
    to
    C:\Users\ehsan\Github\Photos\node_modules\@dfinity\agent\lib\cjs\agent\http\transforms.js

--- components/Auth/Dfinity/auth.ts
+++ components/Auth/Dfinity/auth.ts
@@ -1,3 +1,9 @@
+import 'text-encoding';
+import { HttpAgent, } from "@dfinity/agent";
+
 export const login = async() => {
-    
+    const http = new HttpAgent({
+        host: "https://identity.ic0.app/",
+    });
+    /*http.status().then((res)=>{console.log(res)})*/
 }
