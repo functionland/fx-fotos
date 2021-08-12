17c0cd79b076914ed7bd6c901d5c14a9e8f5f088
ehsan shariati <ehsan6sha@gmail.com>
2021-08-11 22:28:19 -0400

    removed NetInfo as it is not supported by expo
    removed @react-native-community/netinfo from video player

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -2,7 +2,6 @@ import { __rest } from "tslib";
 import { Audio, Video } from 'expo-av';
 import { Animated, Dimensions, Text, TouchableOpacity, TouchableWithoutFeedback, View, } from 'react-native';
 import { FullscreenEnterIcon, FullscreenExitIcon, PauseIcon, PlayIcon, ReplayIcon, Spinner, MuteIcon, UnmuteIcon } from './assets/icons';
-import { useNetInfo } from '@react-native-community/netinfo';
 import { withDefaultProps } from 'with-default-props';
 import React, { useEffect, useState, useRef } from 'react';
 import Slider from '@react-native-community/slider';
@@ -86,7 +85,8 @@ const VideoPlayer = (props) => {
     let hideAnimation = null;
     let shouldPlayAtEndOfSeek = false;
     let controlsTimer = null;
-    const { isConnected } = useNetInfo();
+    //const { isConnected } = useNetInfo();
+    const isConnected = true; //TODO: use a real connection identifier instead of useNetInfo
     const [playbackState, setPlaybackState] = useState(PlaybackStates.Loading);
     const [lastPlaybackStateUpdate, setLastPlaybackStateUpdate] = useState(Date.now());
     const [seekState, setSeekState] = useState(SeekStates.NotSeeking);
