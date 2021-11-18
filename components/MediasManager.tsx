305e3294576740fe0f635a1bcd41f9edbc82ab34
farhoud <farhoud.m7@gmail.com>
2021-11-18 09:56:19 +0330

    upgrade repository and media manager to use cache as default and update it in refresh events

--- components/MediasManager.tsx
+++ components/MediasManager.tsx
@@ -11,40 +11,39 @@ const MediasManager = () => {
 	const [medias,setMedias]=useRecoilState(mediasState)
 	const mediaRepository = new MediaRepository();
 	const mediaReducer = (action: Action) => {
-		if (action.type === "insert") {
-			const insertedMedia = action.payload.map(asset => {
-				return {...asset, cid: '', hasCid: false}
-			})
-			setMedias(currVal => [...currVal, ...insertedMedia])
-		}
-		if (action.type === "delete") {
-			setMedias((currVal => currVal.filter((media) => {
-				if (action.payload.find((asset) => asset.id === media.id)) {
-					return true
-				}
-			})))
-		}
-		if (action.type === "refresh") {
-			console.log("refresh called");
-			(async () => {
-				const data = await mediaRepository.getAll()
-				setMedias(data)
-				console.log("refresh call end");
-				// 
-			})()
-		}
 		if (action.type === "loading") {
-			console.log("loading called");
 			(async () => {
-				const gen = mediaRepository.getIterable()
-				while (true){
-					const {done,value} = await gen.next()
-					if(done){
-						console.log("get all done")
-						break;
+				try{
+					const gen = mediaRepository.getIterable()
+					while (true){
+						// @ts-ignore
+						const {done,value} = await gen.next()
+						if(done){
+							break;
+						}
+						switch (value.type){
+							case 'old':{
+								setMedias((currVal) => [...currVal,...value.medias])
+								break;
+							}
+							case 'cache':{
+								if(medias.length===0){
+									setMedias(value.medias)
+								}
+								break;
+							}
+							case 'new':{
+								setMedias((currVal) => [...value.medias, ...currVal])
+								break;
+							}
+						}
+
+
 					}
-					setMedias((currVal) => [...currVal,...value])
-				}				
+				}catch (e) {
+					console.log(e.message)
+				}
+		
 			})()
 		}
 	}
@@ -53,26 +52,13 @@ const MediasManager = () => {
 			// .then((res) => setPermission(res))
 			.catch((error) => {
 			});
-		MediaLibrary.addListener(event => {
-			console.log(event)
-			if (event.hasIncrementalChanges) {
-				if(event?.insertedAssets.length>0){
-					mediaReducer({type:"insert",payload:event.insertedAssets})
-				}
-				if(event?.deletedAssets.length>0){
-					mediaReducer({type:"delete",payload:event.deletedAssets})
-				}
-			}else {
-				mediaReducer({type:"loading",payload:[]})
-			}
-			
-		})
 		mediaReducer({type:"loading",payload:[]})
+		MediaLibrary.addListener(event => {
+			mediaReducer({type:"loading",payload:[]})
+		})
 		return () => {MediaLibrary.removeAllListeners()}
 	}, [])
-	useEffect(()=>{
-		console.log("medias changed")
-	},[medias])
+
 	return (<></>)
 }
 
--- domian/repositories.ts
+++ domian/repositories.ts
@@ -5,34 +5,31 @@ import {AsyncStorage} from "react-native";
 import {manipulateAsync, SaveFormat} from 'expo-image-manipulator';
 import {PhotoHeight} from "../components/Photos/Constants";
 
+
+interface MediaResponse {
+	type: 'new' | 'cache' | 'old',
+	medias: Media[]
+}
+
 export class MediaRepository {
 	constructor() {
 	}
-	
-	async * getIterable():AsyncIterable<Media[]>{
+
+	async* getIterable(): AsyncIterable<MediaResponse> {
+		if(await getLock()){
+			throw Error('Media Locked')
+		}
+		await setLock(true)
 		const gen = asyncMerge()
-		while (true){
-			const {done,value} = await gen.next()
-			if(done){
-				console.log("get all done")
+		while (true) {
+			// @ts-ignore
+			const {done, value} = await gen.next()
+			if (done) {
 				break;
 			}
 			yield(value)
 		}
-	}
-
-	async getAll(): Promise<Media[]> {
-		const gen = asyncMerge()
-		let all=[];
-		while (true){
-			const {done,value} = await gen.next()
-			if(done){
-				console.log("get all done")
-				break;
-			}
-			all.push(...value)
-		}
-		return  all
+		await setLock(false)
 	}
 
 	async write(medias: Media[]) {
@@ -40,47 +37,25 @@ export class MediaRepository {
 	}
 }
 
-async function* getLocalAssets(after: string = '', size = 9999999999): AsyncIterable<Asset[]> {
-	const getStorageCursor = (
-		limit: number = 99999999999999,
-		after: string = '',
-		createdBefore: Date | number | undefined = undefined,
-		createdAfter: Date | number | undefined = undefined,
-		mediaType: Array<any> = [
+async function* getExternalAssets(options?: MediaLibrary.AssetsOptions, size = 999999): AsyncIterable<Asset[]> {
+	let assetOptions: MediaLibrary.AssetsOptions = {
+		first: 20,
+		mediaType: [
 			MediaLibrary.MediaType.photo,
 			MediaLibrary.MediaType.video,
 		],
-	) => {
-		let mediaFilter: MediaLibrary.AssetsOptions = {
-			mediaType: mediaType,
-			sortBy: [MediaLibrary.SortBy.creationTime],
-		};
-		if (limit) {
-			mediaFilter.first = limit;
-		} else {
-			mediaFilter.first = 9999999999999999;
-		}
-		if (after) {
-			mediaFilter.after = after;
-		}
-		if (createdAfter) {
-			mediaFilter.createdAfter = createdAfter;
-		}
-		if (createdBefore) {
-			mediaFilter.createdBefore = createdBefore;
-		}
-
-		return MediaLibrary.getAssetsAsync(mediaFilter);
-	};
+		sortBy: [MediaLibrary.SortBy.creationTime, MediaLibrary.SortBy.creationTime],
+		...options
+	}
 	let assetPage
 	let flag = 0
 	do {
-		try{
-			assetPage = await getStorageCursor(100, after)
+		try {
+			assetPage = await MediaLibrary.getAssetsAsync(assetOptions)
 			yield assetPage.assets
 			flag += assetPage.assets.length
-			after = assetPage.endCursor
-		}catch (e) {
+			assetOptions.after = assetPage.endCursor
+		} catch (e) {
 			break;
 		}
 
@@ -89,40 +64,75 @@ async function* getLocalAssets(after: string = '', size = 9999999999): AsyncIter
 
 }
 
-async function* asyncMerge(): AsyncIterable<Media[]> {
-	let medias = await getMedias()
+async function* asyncMerge(): AsyncIterable<MediaResponse> {
+	let medias: Media[] = (await getMedias()).sort(
+		function (a:Media, b:Media) {
+			// @ts-ignore
+			return new Date(b.creationTime||b.modificationTime) - new Date(a.creationTime||a.modificationTime);
+		}
+	)
 	let Hashmap = new Map<string, number>()
 	for (const [index, media] of medias.entries()) {
 		Hashmap.set(media.id, index)
 	}
-	const gen = getLocalAssets('',99999999)
-	while (true){
-		const {done, value} = await gen.next()
-		if(done){
-			console.log("get Local asset done")
-			break;
+	if (medias.length > 1) {
+		yield {type: "cache", medias}
+		for (const item of [0, medias.length - 1]) {
+			const newItemIterable = getExternalAssets({[item === 0 ? 'createdAfter' : 'createdBefore']: medias[item].creationTime || medias[item].modificationTime})
+			while (true) {
+				// @ts-ignore
+				const {done, value} = await newItemIterable.next()
+				if (done) {
+					break;
+				}
+				let newMedias: Media[] = []
+				for (const asset of value) {
+					if (!asset.uri || !asset.uri.includes('DCIM') || Hashmap.has(asset.id)) {
+						continue;
+					}
+					let thumbnail = null;
+					if (asset.mediaType === 'photo' && asset.uri)
+						thumbnail = await manipulateAsync(asset.uri, [{'resize': {height: PhotoHeight}}], {
+							compress: 1,
+							format: SaveFormat.PNG
+						})
+					newMedias.push({...asset, cid: '', hasCid: false, preview: thumbnail ? thumbnail.uri : ''})
+				}
+				yield {type: item === 0 ? 'new' : 'old', medias: newMedias}
+			}
 		}
-		let merged = []
-		for (const asset of value) {
-			if(!asset.uri||!asset.uri.includes('DCIM')){
-				continue;
-			}
-			if (Hashmap.has(asset.id)) {
-				merged.push({...asset, ...medias[Hashmap.get(asset.id) as number]})
-			} else {
-				let thumbnail = null;
-				if (asset.mediaType === 'photo'&& asset.uri)
-					thumbnail = await manipulateAsync(asset.uri, [{'resize': {height: PhotoHeight}}], {
-						compress: 1,
-						format: SaveFormat.PNG
-					})
-				// console.log(thumbnail?.uri)
-				merged.push({...asset, cid: '', hasCid: false, preview: thumbnail ? thumbnail.uri : ''})
-			}
 
+
+	} else {
+		const gen = getExternalAssets()
+		while (true) {
+			// @ts-ignore
+			const {done, value} = await gen.next()
+			if (done) {
+				break;
+			}
+			let merged = []
+			for (const asset of value) {
+				if (!asset.uri || !asset.uri.includes('DCIM')) {
+					continue;
+				}
+				if (Hashmap.has(asset.id)) {
+					merged.push({...asset, ...medias[Hashmap.get(asset.id) as number]})
+				} else {
+					let thumbnail = null;
+					if (asset.mediaType === 'photo' && asset.uri)
+						thumbnail = await manipulateAsync(asset.uri, [{'resize': {height: PhotoHeight}}], {
+							compress: 1,
+							format: SaveFormat.PNG
+						})
+					merged.push({...asset, cid: '', hasCid: false, preview: thumbnail ? thumbnail.uri : ''})
+				}
+
+			}
+			yield {type: 'old', medias: merged}
 		}
-		yield merged
 	}
+
 }
 
 
@@ -138,6 +148,23 @@ const getMedias = async () => {
 export const setMedias = async (value: Media[]) => {
 	try {
 		await AsyncStorage.setItem('medias', JSON.stringify(value))
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+const setLock = async (value:boolean)=>{
+	try {
+		await AsyncStorage.setItem('mediaLock', JSON.stringify(value))
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+const getLock = async () => {
+	try {
+		const jsonValue = await AsyncStorage.getItem('mediaLock')
+		return jsonValue != null ? JSON.parse(jsonValue) : false;
 	} catch (e) {
 		console.log(e)
 	}
