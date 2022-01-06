bdcc225aee6141e5e0117485ae3e06c548d840ea
Emad Baqeri <embaqeri1998@gmail.com>
2022-01-07 02:09:02 +0330

    Android API_30 storage request permission #78  - code clean up for asset-item component in order to fix eslint errors

--- app/components/asset-list/recycler-asset-list/asset-items/asset-item.tsx
+++ app/components/asset-list/recycler-asset-list/asset-items/asset-item.tsx
@@ -1,53 +1,51 @@
-import React from "react";
-import { StyleSheet, Image, View } from "react-native";
-import FastImage from "react-native-fast-image";
+import React from "react"
+import { StyleSheet, Image, View } from "react-native"
+import FastImage from "react-native-fast-image"
 
 import { Asset } from "../../../../types"
 import { Checkbox } from "../../.."
+import { palette } from "../../../../theme/palette"
 
 interface Props {
-    asset: Asset,
-    selected:boolean;
-    selectionMode:boolean;
+  asset: Asset
+  selected: boolean
+  selectionMode: boolean
 }
 
 const AssetItem = (props: Props): JSX.Element => {
-    const { asset,selected,selectionMode } = props;
-    return (
-        <View style={styles.container}>
-            {selectionMode?<Checkbox value={selected} style={{position:"absolute",top:5,left:5,zIndex:99}}/>:null}
-            <Image
-                style={styles.image}
-                source={{
-                    uri: asset.uri,
-                    priority: FastImage.priority.normal,
-                }}
-                resizeMode="cover"
-                fadeDuration={100}
-            />
-            {/* <FastImage
-				style={styles.image}
-				source={{
-					uri: asset.uri,
-					priority: FastImage.priority.high,
-                    cache:FastImage.cacheControl.immutable
-				}}
-				resizeMode={FastImage.resizeMode.cover}
-			/> */}
-        </View>
-    )
+  const { asset, selected, selectionMode } = props
+  return (
+    <View style={styles.container}>
+      {selectionMode ? <Checkbox value={selected} style={styles.checkbox} /> : null}
+      <Image
+        style={styles.image}
+        source={{
+          uri: asset.uri,
+          priority: FastImage.priority.normal,
+        }}
+        resizeMode="cover"
+        fadeDuration={100}
+      />
+    </View>
+  )
 }
 const styles = StyleSheet.create({
-    container: {
-        borderColor: 'white',
-        borderWidth: 1,
-        flex: 1
-    },
-    image: {
-        flex: 1,
-        height: undefined,
-        width: undefined
-    },
+  checkbox: {
+    left: 5,
+    position: "absolute",
+    top: 5,
+    zIndex: 99,
+  },
+  container: {
+    borderColor: palette.white,
+    borderWidth: 1,
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    height: undefined,
+    width: undefined,
+  },
 })
 
-export default AssetItem;
+export default AssetItem
