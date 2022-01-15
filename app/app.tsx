fbf84614150f23e8ed27ff1becdb80e5e234e18e
DESKTOP-HAORFNM\Mahdi <mahdi.ghorbani@gmail.com>
2022-01-15 22:54:09 +0330

    select image animation issue is fixed.

--- app/app.tsx
+++ app/app.tsx
@@ -57,7 +57,7 @@ function App() {
   return (
     <ToggleStorybook>
       <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <ErrorBoundary catchErrors={"always"}>
+        <ErrorBoundary catchErrors={"prod"}>
           <AppNavigator onStateChange={onNavigationStateChange} />
         </ErrorBoundary>
       </SafeAreaProvider>
