c0ee7f6b3c600b7da636bf6ad2cd47b9eb2a4cf7
emadbaqeri <ebaqeri@protonmail.com>
2022-03-05 01:33:06 +0330

    panGesture animation for the photo 1. Photo is going to be transparent while doing the gesture 2. Photo is scaling down while doing to gesture 3. Photo will get back to original position if we stop holding it

--- app/navigators/HomeNavigation.tsx
+++ app/navigators/HomeNavigation.tsx
@@ -27,6 +27,21 @@ export const HomeNavigation = () => {
         cardOverlayEnabled: true,
         cardStyle: { backgroundColor: "transparent" },
         animationEnabled: true,
+        cardStyleInterpolator: ({ current: { progress } }) => ({
+          cardStyle: {
+            opacity: progress.interpolate({
+              inputRange: [0, 0.5, 0.9, 1],
+              outputRange: [0, 0.25, 0.7, 1],
+            }),
+          },
+          overlayStyle: {
+            opacity: progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [0, 0],
+              extrapolate: "clamp",
+            }),
+          },
+        }),
       }}
     >
       <Screen name={HomeNavigationTypes.HomeScreen} component={HomeScreen} />
--- app/screens/photo/index.tsx
+++ app/screens/photo/index.tsx
@@ -1,52 +1,83 @@
 import React from "react"
-import { Image, StyleSheet } from "react-native"
+import Animated, {
+  withTiming,
+  interpolate,
+  Extrapolate,
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+} from "react-native-reanimated"
+import { snapPoint } from "react-native-redash"
+import { Dimensions, Image, StyleSheet } from "react-native"
 import { SharedElement } from "react-navigation-shared-element"
 import { PanGestureHandler } from "react-native-gesture-handler"
+import { widthPercentageToDP } from "react-native-responsive-screen"
 import { NavigationProp, RouteProp } from "@react-navigation/native"
-import { HomeNavigationTypes } from "../../navigators/HomeNavigation"
-import Animated, {
-  useAnimatedGestureHandler,
-  useAnimatedStyle,
-  useSharedValue,
-} from "react-native-reanimated"
 
+import { palette } from "../../theme/palette"
 import { RecyclerAssetListSectionData } from "../../types"
-import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen"
+import { HomeNavigationTypes } from "../../navigators/HomeNavigation"
+
+const { height } = Dimensions.get("window")
 
 interface PhotoScreenProps {
   navigation: NavigationProp<HomeNavigationTypes>
   route: RouteProp<{ params: { section: RecyclerAssetListSectionData } }>
 }
 
-export const PhotoScreen: React.FC<PhotoScreenProps> = ({ route, navigation }) => {
+export const PhotoScreen: React.FC<PhotoScreenProps> = ({ route }) => {
   const img: RecyclerAssetListSectionData = route.params.section.data
 
+  const isGestureActive = useSharedValue(false)
   const translateX = useSharedValue(0)
   const translateY = useSharedValue(0)
 
   const animatedStyle = useAnimatedStyle(() => {
+    const scale = interpolate(translateY.value, [0, height], [1, 0.5], Extrapolate.CLAMP)
     return {
       alignItems: "center",
       flex: 1,
       justifyContent: "center",
-      transform: [{ translateX: translateX.value }, { translateY: translateY.value }],
+      transform: [{ translateX: translateX.value }, { translateY: translateY.value }, { scale }],
     }
   })
 
   const onPanGesture = useAnimatedGestureHandler({
+    onStart: () => {
+      isGestureActive.value = true
+    },
     onActive: ({ translationX, translationY }) => {
       translateX.value = translationX
       translateY.value = translationY
     },
-    onEnd: ({ velocityX, velocityY }) => {},
+    onEnd: ({ velocityY }) => {
+      const shouldGoBack = snapPoint(translateY.value, velocityY, [0, height]) === height
+      if (shouldGoBack) {
+        // TODO: While trying to go back app crashes :/
+      } else {
+        translateX.value = withTiming(0, { duration: 100 })
+        translateY.value = withTiming(0, { duration: 100 })
+      }
+      isGestureActive.value = false
+    },
+  })
+
+  const wrapperStyle = useAnimatedStyle(() => {
+    return {
+      flex: 1,
+      backgroundColor: palette.white,
+      opacity: withTiming(isGestureActive.value ? 0.7 : 1),
+    }
   })
 
   return (
     <PanGestureHandler onGestureEvent={onPanGesture}>
-      <Animated.View style={animatedStyle}>
-        <SharedElement style={styles.container} id={img.uri}>
-          <Image source={{ uri: img.uri }} style={styles.image} />
-        </SharedElement>
+      <Animated.View style={wrapperStyle}>
+        <Animated.View style={animatedStyle}>
+          <SharedElement style={styles.container} id={img.uri}>
+            <Image source={{ uri: img.uri }} style={styles.image} />
+          </SharedElement>
+        </Animated.View>
       </Animated.View>
     </PanGestureHandler>
   )
