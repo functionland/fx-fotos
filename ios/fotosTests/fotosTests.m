b4b63c681991d6b1039491953087f4fa5d962680
DESKTOP-HAORFNM\Mahdi <mahdi.ghorbani@gmail.com>
2022-03-30 16:50:58 +0430

    refactor photos to fotos in IOS project

--- ios/fotosTests/fotosTests.m
+++ ios/fotosTests/fotosTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface photosTests : XCTestCase
+@interface fotosTests : XCTestCase
 
 @end
 
-@implementation photosTests
+@implementation fotosTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
