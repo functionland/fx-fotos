b024b513e44da186287b9bd2bb129b9ce6fe4fae
Mati Q <mahdi.ghorbani@gmail.com>
2022-04-23 16:27:10 +0430

    upgrade navigation, gesture pcks

--- android/app/src/main/java/land/fx/fotos/MainActivity.java
+++ android/app/src/main/java/land/fx/fotos/MainActivity.java
@@ -4,7 +4,7 @@ import com.facebook.react.ReactActivity;
 
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+//import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import expo.modules.ReactActivityDelegateWrapper;
 import android.os.Bundle;
@@ -26,16 +26,16 @@ public class MainActivity extends ReactActivity {
    * Required for proper react-native-gesture-handler touch handling
    * https://docs.swmansion.com/react-native-gesture-handler/docs/#updating-mainactivityjava
    */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(
-      this,
-      new ReactActivityDelegate(this, getMainComponentName()) {
-        @Override
-        protected ReactRootView createRootView() {
-          return new RNGestureHandlerEnabledRootView(MainActivity.this);
-        }
-      }
-    );
-  }
+  // @Override
+  // protected ReactActivityDelegate createReactActivityDelegate() {
+  //   return new ReactActivityDelegateWrapper(
+  //     this,
+  //     new ReactActivityDelegate(this, getMainComponentName()) {
+  //       @Override
+  //       protected ReactRootView createRootView() {
+  //         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+  //       }
+  //     }
+  //   );
+  // }
 }
