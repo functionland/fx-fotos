78bc4a3f9151b9586a7ab99a223935d41f5d014a
Mati Q <mahdi.ghorbani@gmail.com>
2022-06-25 19:57:29 +0430

    Using another way instead of rn-nodeify

--- shim.js
+++ shim.js
@@ -1,27 +1,3 @@
-if (typeof __dirname === 'undefined') global.__dirname = '/'
-if (typeof __filename === 'undefined') global.__filename = ''
-if (typeof process === 'undefined') {
-  global.process = require('process')
-} else {
-  const bProcess = require('process')
-  for (var p in bProcess) {
-    if (!(p in process)) {
-      process[p] = bProcess[p]
-    }
-  }
-}
-
-process.browser = false
-if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
+// eslint-disable-next-line no-undef
 if (typeof BigInt === 'undefined') global.BigInt = require('big-integer');
 
-// global.location = global.location || { port: 80 }
-const isDev = typeof __DEV__ === 'boolean' && __DEV__
-process.env['NODE_ENV'] = isDev ? 'development' : 'production'
-if (typeof localStorage !== 'undefined') {
-  localStorage.debug = isDev ? '*' : ''
-}
-
-// If using the crypto shim, uncomment the following line to ensure
-// crypto is loaded first, so it can populate global.crypto
-// require('crypto')
