156a4cf5ea61e822e178a5d48c6b4e40f9011ca3
Keith Luchtel <keith@keithluchtel.com>
2022-06-29 16:30:40 -0500

    Use reanimated 3.0 rc
    Updates reanimated to 3.0 which has support for RN 0.69 and builds
    on Android. Also adds react-native-gesture-handler.

--- apps/box/src/app/App.tsx
+++ apps/box/src/app/App.tsx
@@ -5,6 +5,8 @@ import { RootNavigator } from '../navigation/Root.navigator';
 import { WalletConnectProvider } from '@walletconnect/react-native-dapp/dist/providers';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavContainer } from '../navigation/NavContainer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 
 export const App = () => {
   const [isDarkTheme] = React.useState<boolean>(false);
@@ -18,7 +20,9 @@ export const App = () => {
           asyncStorage: AsyncStorage,
         }}
       >
-        <AppContent />
+        <GestureHandlerRootView style={styles.flex1}>
+          <AppContent />
+        </GestureHandlerRootView>
       </WalletConnectProvider>
     </ThemeProvider>
   );
@@ -31,5 +35,11 @@ const AppContent = () => {
     </NavContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  flex1: {
+    flex: 1,
+  },
+});
 
 export default App;
