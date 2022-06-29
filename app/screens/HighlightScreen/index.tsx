f634493fef7cd06076bde738c7d32ee34aa9f098
Emad Baqeri <ebaqeri@protonmail.com>
2022-06-29 18:07:49 +0430

    Add feature next/previous story

--- app/screens/HighlightScreen/index.tsx
+++ app/screens/HighlightScreen/index.tsx
@@ -1,19 +1,19 @@
-import "react-native-get-random-values"
 import * as React from "react"
-import "react-native-get-random-values"
+import shortid from "shortid"
 import { RouteProp } from "@react-navigation/native"
-import { LongPressGestureHandler } from "react-native-gesture-handler"
-import { View, StyleSheet, ImageBackground, Image, TouchableOpacity } from "react-native"
+import { View, StyleSheet, TouchableOpacity } from "react-native"
+import { FlatList, LongPressGestureHandler } from "react-native-gesture-handler"
 import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen"
 import Animated, {
   runOnJS,
-  withDelay,
   withTiming,
   useSharedValue,
   useAnimatedStyle,
   useAnimatedReaction,
+  SharedValue,
 } from "react-native-reanimated"
 import { withPause } from "react-native-redash"
+import FastImage from "react-native-fast-image"
 
 import { AssetStory } from "../../types"
 
@@ -26,69 +26,54 @@ const ANIMATION_DURATION = 1500
 const OPACITY_FADE_DURATION = 111
 
 interface timeBarProps {
-  numberOfBars: number
-  imageIdx: Animated.SharedValue<number>
-  pauseAnimation: Animated.SharedValue<boolean>
-  backgroundImageIdx: Animated.SharedValue<number>
-  setTimeBarItems: React.Dispatch<React.SetStateAction<number[]>>
+  width: number
+  pause: SharedValue<boolean>
 }
 
-const TimeBar: React.FC<timeBarProps> = ({
-  numberOfBars,
-  imageIdx,
-  pauseAnimation,
-  backgroundImageIdx,
-  setTimeBarItems,
-}) => {
+const TimeBar: React.FC<timeBarProps> = ({ width, pause }) => {
   const barWidth = useSharedValue(0)
-  const BAR_WIDTH = (widthPercentageToDP(95) - 2 * numberOfBars) / numberOfBars
 
-  const timeBarAnimatedStyle = useAnimatedStyle(() => {
+  const animatedStyle = useAnimatedStyle(() => {
     return {
       width: barWidth.value,
     }
   })
 
+  React.useLayoutEffect(() => {
+    barWidth.value = withPause(
+      withTiming(width, {
+        duration: ANIMATION_DURATION - DELAY_DURATION,
+      }),
+      pause,
+    )
+  }, [])
+
+  return <Animated.View style={[styles.timeBar, animatedStyle]} />
+}
+
+export const HighlightScreen: React.FC<HighlightScreenProps> = ({ route }) => {
+  const { data } = route.params.highlights
+
+  const [imageIdx, setImageIdx] = React.useState(0)
+  const pauseAnimation = useSharedValue(false)
+  const timeBarContainerOpacity = useSharedValue(1)
+  const highlightListRef = React.useRef<FlatList>(null)
+  const [timeBarItems, setTimeBarItems] = React.useState<number[]>([0])
+
+  const barWidth = useSharedValue(0)
+  const BAR_WIDTH = (widthPercentageToDP(95) - 2 * data.length) / data.length
+
   const updateImage = () => {
-    if (imageIdx.value >= numberOfBars - 1) {
+    if (imageIdx >= data.length - 1) {
       return
     }
 
-    imageIdx.value += 1
-    setTimeBarItems((prev) => [...prev, imageIdx.value + 1])
-  }
-
-  const updateBackgroundImage = () => {
-    if (backgroundImageIdx.value >= numberOfBars - 1) {
-      return
-    }
-
-    backgroundImageIdx.value += 1
+    setImageIdx((prev) => (prev += 1))
+    setTimeBarItems((prev) => [...prev, imageIdx + 1])
   }
 
   React.useLayoutEffect(() => {
     barWidth.value = withPause(
-      withDelay(
-        DELAY_DURATION,
-        withTiming(
-          BAR_WIDTH,
-          {
-            duration: ANIMATION_DURATION,
-          },
-          function (isFinished) {
-            if (isFinished) {
-              runOnJS(updateBackgroundImage)()
-            }
-          },
-        ),
-      ),
-      pauseAnimation,
-    )
-  }, [])
-
-  const _unused = useSharedValue(0)
-  React.useLayoutEffect(() => {
-    _unused.value = withPause(
       withTiming(
         BAR_WIDTH,
         {
@@ -102,21 +87,11 @@ const TimeBar: React.FC<timeBarProps> = ({
       ),
       pauseAnimation,
     )
-  }, [])
+  }, [imageIdx])
 
-  return <Animated.View style={[styles.timeBar, timeBarAnimatedStyle]} />
-}
-
-// TODO: Next & Previous Story by tapping left or right
-export const HighlightScreen: React.FC<HighlightScreenProps> = ({ route }) => {
-  const { data } = route.params.highlights
-
-  const imageIdx = useSharedValue(0)
-  const backgroundImageIdx = useSharedValue(0)
-  const pauseAnimation = useSharedValue(false)
-  const timeBarContainerOpacity = useSharedValue(1)
-
-  const [timeBarItems, setTimeBarItems] = React.useState<number[]>([0])
+  React.useEffect(() => {
+    highlightListRef.current.scrollToIndex({ animated: true, index: imageIdx })
+  }, [imageIdx])
 
   const timeBarContainerAnimatedStyle = useAnimatedStyle(() => {
     return {
@@ -141,59 +116,77 @@ export const HighlightScreen: React.FC<HighlightScreenProps> = ({ route }) => {
       onEnded={() => (pauseAnimation.value = false)}
     >
       <View style={{ backgroundColor: "#000" }}>
-        <View
-          style={{
-            position: "absolute",
-            height: heightPercentageToDP(100),
-            width: widthPercentageToDP(100),
+        <FlatList
+          ref={highlightListRef}
+          scrollEnabled={false}
+          horizontal={true}
+          data={data}
+          keyExtractor={() => shortid.generate()}
+          renderItem={({ item }) => {
+            return (
+              <FastImage
+                resizeMode={FastImage.resizeMode.cover}
+                source={{ uri: item.uri, priority: FastImage.priority.high }}
+                style={{ height: heightPercentageToDP(100), width: widthPercentageToDP(100) }}
+              />
+            )
           }}
-        >
-          <Image
-            source={{ uri: data[backgroundImageIdx.value].uri }}
-            style={{
-              height: "100%",
-              width: "100%",
+          getItemLayout={(data, index) => ({
+            length: data.length * widthPercentageToDP(100),
+            offset: widthPercentageToDP(100) * index,
+            index,
+          })}
+        />
+        <Animated.View style={[styles.timeBarContainer, timeBarContainerAnimatedStyle]}>
+          <FlatList
+            horizontal
+            keyExtractor={(item) => item.toString()}
+            data={timeBarItems}
+            renderItem={() => {
+              return <TimeBar width={BAR_WIDTH} pause={pauseAnimation} />
             }}
           />
-        </View>
-        <ImageBackground
-          source={{ uri: data[imageIdx.value].uri }}
-          style={{ height: "100%", width: "100%" }}
-        >
-          <Animated.View style={[styles.timeBarContainer, timeBarContainerAnimatedStyle]}>
-            {timeBarItems.map((item) => {
-              return (
-                <TimeBar
-                  key={item}
-                  setTimeBarItems={setTimeBarItems}
-                  backgroundImageIdx={backgroundImageIdx}
-                  pauseAnimation={pauseAnimation}
-                  numberOfBars={data.length}
-                  imageIdx={imageIdx}
-                />
-              )
-            })}
-          </Animated.View>
-          <Animated.View style={[styles.timeBarContainer, timeBarContainerAnimatedStyle]}>
-            {data.map((item) => {
+        </Animated.View>
+        <Animated.View style={[styles.timeBarContainer, timeBarContainerAnimatedStyle]}>
+          <FlatList
+            horizontal
+            keyExtractor={(item) => item.id}
+            data={data}
+            renderItem={() => {
               return (
                 <View
-                  key={item.id}
                   style={[
-                    styles.timeBar,
+                    styles.timeBarPlaceholder,
                     {
-                      zIndex: 900,
-                      backgroundColor: "rgba(0,0,0, 0.1)",
                       width: (widthPercentageToDP(95) - 2 * data.length) / data.length,
                     },
                   ]}
                 />
               )
-            })}
-          </Animated.View>
-          <TouchableOpacity style={styles.pressableContainerLeft} />
-          <TouchableOpacity style={styles.pressableContainerRight} />
-        </ImageBackground>
+            }}
+          />
+        </Animated.View>
+        <TouchableOpacity
+          onPress={() => {
+            if (imageIdx <= 0) {
+              return
+            }
+            setImageIdx((prev) => (prev -= 1))
+            setTimeBarItems((prev) => prev.slice(0, -1))
+            barWidth.value = BAR_WIDTH
+          }}
+          style={[styles.pressableContainer, { left: 0 }]}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            if (imageIdx >= data.length - 1) {
+              return
+            }
+            setImageIdx((prev) => (prev += 1))
+            setTimeBarItems((prev) => [...prev, imageIdx + 1])
+          }}
+          style={[styles.pressableContainer, { right: 0 }]}
+        />
       </View>
     </LongPressGestureHandler>
   )
@@ -219,27 +212,19 @@ const styles = StyleSheet.create({
     backgroundColor: "grey",
     marginLeft: 2,
   },
-  pressibleContainers: {
-    position: "absolute",
-    height: heightPercentageToDP(100),
-    width: widthPercentageToDP(30),
+  timeBarPlaceholder: {
+    height: 4,
+    marginTop: 40,
+    borderRadius: 100,
+    marginLeft: 2,
+    zIndex: 900,
+    backgroundColor: "rgba(0,0,0, 0.1)",
   },
-  pressableContainerRight: {
+  pressableContainer: {
     top: 0,
     bottom: 0,
-    right: 0,
     position: "absolute",
     height: heightPercentageToDP(100),
     width: widthPercentageToDP(20),
-    backgroundColor: "rgba(0,0,0, .2)",
-  },
-  pressableContainerLeft: {
-    top: 0,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-    height: heightPercentageToDP(100),
-    width: widthPercentageToDP(20),
-    backgroundColor: "rgba(0,0,0, .2)",
   },
 })
