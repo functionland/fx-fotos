ced4f6544aac54635584209a35de49071a35d74e
Keith Luchtel <keith@keithluchtel.com>
2022-07-05 13:03:19 -0500

    Add support for button variants
    Add system for defining different styles of reusable button that
    can be reused and kept consistent.

--- libs/component-library/src/lib/button/button.tsx
+++ libs/component-library/src/lib/button/button.tsx
@@ -3,23 +3,44 @@
  */
 
 import React from 'react';
-import { createBox } from '@shopify/restyle';
+import {
+  createBox,
+  createRestyleComponent,
+  createVariant,
+  VariantProps,
+} from '@shopify/restyle';
 import { Pressable, PressableProps } from 'react-native';
 import { FxTheme } from '../theme';
 import { FxText } from '../text/text';
 
-const FxButtonBase = createBox<FxTheme, PressableProps>(Pressable);
+const buttonVariant = createVariant({ themeKey: 'buttonVariants' });
+const PressableBox = createBox<FxTheme, PressableProps>(Pressable);
 
-type FxButtonProps = React.ComponentProps<typeof FxButtonBase> & {
+const FxButtonBase = createRestyleComponent<
+  React.ComponentProps<typeof PressableBox> &
+    VariantProps<FxTheme, 'buttonVariants'>,
+  FxTheme
+>([buttonVariant], PressableBox);
+
+type FxButtonProps = Omit<
+  React.ComponentProps<typeof FxButtonBase>,
+  'variant'
+> & {
+  variant?: keyof typeof ButtonVariants;
   children?: React.ReactNode | string;
 };
 
-const FxButton = ({ children, style, ...rest }: FxButtonProps) => {
+const FxButton = ({
+  variant = 'default',
+  children,
+  style,
+  ...rest
+}: FxButtonProps) => {
   return (
     <FxButtonBase
+      variant={ButtonVariants[variant].buttonVariant}
       padding="m"
       margin="m"
-      backgroundColor="primary"
       borderRadius="s"
       style={(args) => [
         typeof style === 'function' ? style(args) : style,
@@ -29,11 +50,33 @@ const FxButton = ({ children, style, ...rest }: FxButtonProps) => {
       ]}
       {...rest}
     >
-      <FxText variant="body" color="white">
-        {children}
-      </FxText>
+      <FxText {...ButtonVariants[variant].text}>{children}</FxText>
     </FxButtonBase>
   );
+};
+
+type ButtonVariantType = {
+  buttonVariant: React.ComponentProps<typeof FxButtonBase>['variant'];
+  text: Pick<React.ComponentProps<typeof FxText>, 'variant' | 'color'>;
+};
+
+// BUTTON VARIANTS DEFINITIONS
+
+const ButtonVariants: Record<'default' | 'inverted', ButtonVariantType> = {
+  default: {
+    buttonVariant: undefined,
+    text: {
+      variant: 'body',
+      color: 'white',
+    },
+  },
+  inverted: {
+    buttonVariant: 'inverted',
+    text: {
+      variant: 'body',
+      color: 'primary',
+    },
+  },
 };
 
 export { FxButton };
