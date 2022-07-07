aa2c43a80beae5e464eb2326d3f0a79de1421757
Keith Luchtel <keith@keithluchtel.com>
2022-07-07 16:29:14 -0500

    Add FxSafeAreaBox component
    Adds Restyle SafeAreaView component.

--- libs/component-library/src/index.ts
+++ libs/component-library/src/index.ts
@@ -1,6 +1,7 @@
 export { FxBox } from './lib/box/box';
 export { FxButton } from './lib/button/button';
 export { FxPicker, FxPickerItem } from './lib/picker/picker';
+export { FxSafeAreaBox } from './lib/safe-area-box/safeAreaBox';
 export { FxText } from './lib/text/text';
 export { FxTextInput } from './lib/input/input';
 export { FxTheme, fxDarkTheme, fxLightTheme } from './lib/theme/theme';
