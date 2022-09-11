5c81f7ece8b5ecba0332fa19912dcdf6a7d7562e
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2022-09-11 15:01:04 +0200

    SafeArea issue is fixed.

--- app/navigators/home-navigator.tsx
+++ app/navigators/home-navigator.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useTheme } from "@react-navigation/native"
-import { View, Platform, SafeAreaView } from "react-native"
+import { View, Platform } from "react-native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
@@ -8,7 +8,7 @@ import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
 import { HomeScreen, LibraryScreen } from "../screens"
 import { TabHeader } from "../components/header/tab-header"
 import { UnderConstruction } from "../components"
-import { RneDarkTheme } from "../theme"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 export type HomeNavigationParamList = {
   HomeScreen: undefined
 }
@@ -29,96 +29,90 @@ function UnderConstructionScreen() {
 const HomeTabs = createBottomTabNavigator()
 export function HomeNavigator() {
   const { colors, dark } = useTheme()
+  const insets = useSafeAreaInsets()
 
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor: dark ? RneDarkTheme.darkColors.platform.ios.primary : "#fff",
-        height: "100%",
-        width: "100%",
+
+    <HomeTabs.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerShown: false,
+        header: (props) => <TabHeader {...props} />,
+        tabBarStyle: {
+          height: Platform.OS === "ios" ? insets.bottom + 60 : insets.bottom + 70,
+        },
+        tabBarLabelStyle: {
+          fontSize: 15,
+          fontWeight: "600",
+          padding: 5,
+        },
       }}
     >
-      <HomeTabs.Navigator
-        screenOptions={{
-          headerTransparent: true,
-          headerShown: false,
-          header: (props) => <TabHeader {...props} />,
-          tabBarStyle: {
-            height: Platform.OS === "ios" ? 90 : 70,
-          },
-          tabBarLabelStyle: {
-            fontSize: 15,
-            fontWeight: "600",
-            padding: 5,
+      <HomeTabs.Screen
+        options={{
+          tabBarLabel: "Photos",
+          tabBarIcon: function tabIcon(props) {
+            return (
+              <FontAwesome5
+                name={"images"}
+                size={25}
+                color={props?.focused ? colors.text : "gray"}
+              />
+            )
           },
         }}
-      >
-        <HomeTabs.Screen
-          options={{
-            tabBarLabel: "Photos",
-            tabBarIcon: function tabIcon(props) {
-              return (
-                <FontAwesome5
-                  name={"images"}
-                  size={25}
-                  color={props?.focused ? colors.text : "gray"}
-                />
-              )
-            },
-          }}
-          name={HomeNavigationTypes.PhotosTab}
-          component={HomeScreen}
-        />
-        <HomeTabs.Screen
-          name="Search"
-          options={{
-            tabBarIcon: function tabIcon(props) {
-              return (
-                <FontAwesome5
-                  name={"search"}
-                  color={props?.focused ? colors.text : "gray"}
-                  size={25}
-                  style={{}}
-                />
-              )
-            },
-          }}
-          component={UnderConstructionScreen}
-        />
-        <HomeTabs.Screen
-          name="Sharing"
-          options={{
-            tabBarIcon: function tabIcon(props) {
-              return (
-                <FontAwesome5
-                  name={"user-friends"}
-                  color={props?.focused ? colors.text : "gray"}
-                  size={25}
-                  style={{}}
-                />
-              )
-            },
-          }}
-          component={UnderConstructionScreen}
-        />
-        <HomeTabs.Screen
-          name={HomeNavigationTypes.LibraryTab}
-          options={{
-            tabBarLabel: "Library",
-            tabBarIcon: function tabIcon(props) {
-              return (
-                <FontAwesome5
-                  name={"swatchbook"}
-                  color={props?.focused ? colors.text : "gray"}
-                  size={25}
-                  style={{}}
-                />
-              )
-            },
-          }}
-          component={LibraryScreen}
-        />
-      </HomeTabs.Navigator>
-    </SafeAreaView>
+        name={HomeNavigationTypes.PhotosTab}
+        component={HomeScreen}
+      />
+      <HomeTabs.Screen
+        name="Search"
+        options={{
+          tabBarIcon: function tabIcon(props) {
+            return (
+              <FontAwesome5
+                name={"search"}
+                color={props?.focused ? colors.text : "gray"}
+                size={25}
+                style={{}}
+              />
+            )
+          },
+        }}
+        component={UnderConstructionScreen}
+      />
+      <HomeTabs.Screen
+        name="Sharing"
+        options={{
+          tabBarIcon: function tabIcon(props) {
+            return (
+              <FontAwesome5
+                name={"user-friends"}
+                color={props?.focused ? colors.text : "gray"}
+                size={25}
+                style={{}}
+              />
+            )
+          },
+        }}
+        component={UnderConstructionScreen}
+      />
+      <HomeTabs.Screen
+        name={HomeNavigationTypes.LibraryTab}
+        options={{
+          tabBarLabel: "Library",
+          tabBarIcon: function tabIcon(props) {
+            return (
+              <FontAwesome5
+                name={"swatchbook"}
+                color={props?.focused ? colors.text : "gray"}
+                size={25}
+                style={{}}
+              />
+            )
+          },
+        }}
+        component={LibraryScreen}
+      />
+    </HomeTabs.Navigator>
   )
 }
