d1cac5259985a566980a9282cc0c8a281bff4d88
Mati Q <mahdi.ghorbani@gmail.com>
2022-10-10 21:58:01 +0330

    work on search-options animations

--- app/components/search-options-list/search-options-list.tsx
+++ app/components/search-options-list/search-options-list.tsx
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, ViewStyle } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import { Chip } from '@rneui/themed'
+import Animated, { SlideInUp } from 'react-native-reanimated'
+import { Chip, useTheme } from '@rneui/themed'
 import { SearchOptionType, SearchOptionValueType } from '../../types'
 
 interface Props {
   onOptionsPress?: (optionValue: SearchOptionValueType) => void
   selectedOptions?: SearchOptionValueType[]
+  style: ViewStyle
 }
+const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView)
 export const SearchOptionsList = ({
   selectedOptions,
   onOptionsPress,
+  style,
 }: Props) => {
+  const { theme } = useTheme()
   const optionsObj: Record<string, OptionsList> = SearchOptions.reduce(
     (obj, option) => {
       obj[option.order] = option
       return obj
     },
-    [],
+    {},
   )
   const selectedOptionsObj: Record<string, SearchOptionValueType> =
     selectedOptions?.reduce((obj, option) => {
@@ -26,40 +31,138 @@ export const SearchOptionsList = ({
       return obj
     }, {})
 
+  const renderClip = useCallback(
+    (option: SearchOptionValueType) => {
+      switch (option.type) {
+        case 'AssetDateRange':
+          return (
+            <Chip
+              key={option.id}
+              title={option.title}
+              titleStyle={{ color: theme.colors.greyOutline }}
+              type="outline"
+              containerStyle={styles.clipContainerStyle}
+              buttonStyle={[
+                styles.clipButtonStyle,
+                {
+                  borderColor: theme.colors.greyOutline,
+                },
+              ]}
+              icon={{
+                name: 'calendar',
+                type: 'font-awesome',
+                size: 18,
+                color: '#42a5f5',
+              }}
+              onPress={() => onOptionsPress?.(option)}
+            />
+          )
+        case 'AssetType':
+          return (
+            <Chip
+              key={option.id}
+              title={option.title}
+              titleStyle={{ color: theme.colors.greyOutline }}
+              type="outline"
+              containerStyle={styles.clipContainerStyle}
+              buttonStyle={[
+                styles.clipButtonStyle,
+                {
+                  borderColor: theme.colors.greyOutline,
+                },
+              ]}
+              icon={{
+                name: option.icon,
+                type: 'font-awesome',
+                size: 18,
+                color: '#fdd835',
+              }}
+              onPress={() => onOptionsPress?.(option)}
+            />
+          )
+        case 'AssetMime':
+          return (
+            <Chip
+              key={option.id}
+              title={option.title}
+              titleStyle={{ color: theme.colors.greyOutline }}
+              type="outline"
+              containerStyle={styles.clipContainerStyle}
+              buttonStyle={[
+                styles.clipButtonStyle,
+                {
+                  borderColor: theme.colors.greyOutline,
+                },
+              ]}
+              icon={{
+                name: option.icon,
+                type: 'font-awesome',
+                size: 18,
+                color: '#757575',
+              }}
+              onPress={() => onOptionsPress?.(option)}
+            />
+          )
+        case 'AssetDuration':
+          return (
+            <Chip
+              key={option.id}
+              title={option.title}
+              titleStyle={{ color: theme.colors.greyOutline }}
+              type="outline"
+              containerStyle={styles.clipContainerStyle}
+              buttonStyle={[
+                styles.clipButtonStyle,
+                {
+                  borderColor: theme.colors.greyOutline,
+                },
+              ]}
+              icon={{
+                name: option.icon,
+                type: 'material-community',
+                size: 18,
+                color: '#4caf50',
+              }}
+              onPress={() => onOptionsPress?.(option)}
+            />
+          )
+        default:
+          break
+      }
+    },
+    [onOptionsPress],
+  )
   return (
     <ScrollView
       showsVerticalScrollIndicator={false}
       showsHorizontalScrollIndicator={false}
+      style={[{ backgroundColor: theme.colors.background }, style]}
     >
-      {Object.values(optionsObj).map(optionList => (
-        <ScrollView
+      {Object.values(optionsObj).map((optionList, index) => (
+        <AnimatedScrollView
+          entering={SlideInUp.duration(600).delay(index * 0)}
           key={optionList.order}
           horizontal={true}
           showsVerticalScrollIndicator={false}
           showsHorizontalScrollIndicator={false}
         >
-          {optionList.searchOptions?.map(option => {
-            return (
-              <Chip
-                key={option.id}
-                title={option.title}
-                type="outline"
-                containerStyle={{ marginVertical: 15 }}
-                icon={{
-                  name: 'bluetooth',
-                  type: 'font-awesome',
-                  size: 20,
-                  color: 'white',
-                }}
-              />
-            )
-          })}
-        </ScrollView>
+          {optionList.searchOptions?.map(option => renderClip(option))}
+        </AnimatedScrollView>
       ))}
     </ScrollView>
   )
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  clipContainerStyle: {
+    marginVertical: 10,
+    marginHorizontal: 4,
+    borderRadius: 15,
+  },
+  clipButtonStyle: {
+    borderWidth: 1,
+    padding: 5,
+  },
+})
 
 type OptionsList = {
   order: number
@@ -91,7 +194,7 @@ const SearchOptions: OptionsList[] = [
       },
       {
         id: '6 month ago',
-        title: '3 month ago',
+        title: '6 month ago',
         type: 'AssetDateRange',
         icon: 'calendar',
       },
@@ -102,18 +205,93 @@ const SearchOptions: OptionsList[] = [
     type: 'AssetType',
     searchOptions: [
       {
-        id: 'Video',
-        title: 'Video',
-        value: 'video',
-        type: 'AssetType',
-        icon: 'video',
-      },
-      {
-        id: 'Image',
-        title: 'Image',
+        id: 'Images',
+        title: 'Images',
         value: 'photo',
         type: 'AssetType',
-        icon: 'photos',
+        icon: 'file-photo-o',
+      },
+      {
+        id: 'Videos',
+        title: 'Videos',
+        value: 'video',
+        type: 'AssetType',
+        icon: 'video-camera',
+      },
+    ],
+  },
+  {
+    order: 2,
+    type: 'AssetMime',
+    searchOptions: [
+      {
+        id: 'PNG',
+        title: 'PNG',
+        value: 'image/png',
+        type: 'AssetMime',
+        icon: 'photo',
+      },
+      {
+        id: 'JPEG',
+        title: 'JPEG',
+        value: 'image/jpeg',
+        type: 'AssetMime',
+        icon: 'photo',
+      },
+      {
+        id: 'GIF',
+        title: 'GIF',
+        value: 'image/gif',
+        type: 'AssetMime',
+        icon: 'photo',
+      },
+      {
+        id: 'MP4',
+        title: 'MP4',
+        value: 'image/mp4',
+        type: 'AssetMime',
+        icon: 'file-video-o',
+      },
+    ],
+  },
+  {
+    order: 3,
+    type: 'AssetDuration',
+    searchOptions: [
+      {
+        id: 'More than 30s',
+        title: 'More than 30s',
+        value: 30 * 100,
+        type: 'AssetDuration',
+        icon: 'clock-time-ten-outline',
+      },
+      {
+        id: 'More than 60s',
+        title: 'More than 60s',
+        value: 60 * 1000,
+        type: 'AssetDuration',
+        icon: 'clock-time-ten-outline',
+      },
+      {
+        id: 'More than 5min',
+        title: 'More than 5min',
+        value: 5 * 60 * 1000,
+        type: 'AssetDuration',
+        icon: 'clock-time-ten-outline',
+      },
+      {
+        id: 'More than 30 mins',
+        title: 'More than 30 mins',
+        value: 30 * 60 * 1000,
+        type: 'AssetDuration',
+        icon: 'clock-time-ten-outline',
+      },
+      {
+        id: 'More than 60 mins',
+        title: 'More than 60 mins',
+        value: 60 * 60 * 1000,
+        type: 'AssetDuration',
+        icon: 'clock-time-ten-outline',
       },
     ],
   },
--- app/screens/search/index.tsx
+++ app/screens/search/index.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react'
-import { Alert, Platform, StyleSheet, View } from 'react-native'
+import {
+  Alert,
+  Platform,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from 'react-native'
 import { useRecoilState } from 'recoil'
 import { request, PERMISSIONS, openSettings } from 'react-native-permissions'
-import { SearchBar } from '@rneui/themed'
+import { Input, SearchBar } from '@rneui/themed'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { AssetService } from '../../services'
 import {
@@ -11,47 +18,84 @@ import {
 } from '../../navigators/home-navigator'
 import { mediasState } from '../../store'
 import { NavigationProp, RouteProp } from '@react-navigation/native'
-import { Screen, SearchOptionsList } from '../../components'
+import { Header, Screen, SearchOptionsList } from '../../components'
 import AssetList from '../../components/asset-list'
+import Animated, {
+  FadeIn,
+  FadeOut,
+  SlideOutDown,
+  SlideOutUp,
+} from 'react-native-reanimated'
+import { AssetListScreen } from '../asset-list/asset-list-screen'
+import { useFloatHederAnimation } from '../../utils/hooks'
 
 interface SearchScreenProps {
   navigation: NavigationProp<HomeNavigationParamList>
   route: RouteProp<HomeNavigationParamList, HomeNavigationTypes.SearchScreen>
 }
-
 export const SearchScreen: React.FC<SearchScreenProps> = ({ navigation }) => {
+  const [medias] = useRecoilState(mediasState)
+  const [showSearchOptions, setShowSearchOptions] = useState(false)
+  const [scrollY, headerStyles] = useFloatHederAnimation(60)
+  const renderHeader = (
+    style?: StyleProp<Animated.AnimateStyle<StyleProp<ViewStyle>>>,
+  ) => (
+    <Animated.View
+      style={[
+        {
+          position: 'absolute',
+          width: '100%',
+          zIndex: 99,
+          overflow: 'visible',
+        },
+        headerStyles,
+      ]}
+    >
+      <View style={{ flex: 1 }}>
+        <SearchBar
+          style={{ borderWidth: 0 }}
+          placeholder="Search assets ..."
+          containerStyle={{ backgroundColor: 'black', borderWidth: 0 }}
+          inputContainerStyle={{ borderRadius: 50 }}
+          //   onChangeText={updateSearch}
+          //   value={search}
+          onFocus={() => setShowSearchOptions(true)}
+        />
+      </View>
+      {showSearchOptions && (
+        <Animated.View
+          entering={FadeIn.duration(100)}
+          exiting={FadeOut.duration(300)}
+          style={{ flex: 1, height: 800, zIndex: 99 }}
+        >
+          <SearchOptionsList
+            style={{ paddingHorizontal: 10 }}
+            onOptionsPress={() => setShowSearchOptions(false)}
+          />
+        </Animated.View>
+      )}
+    </Animated.View>
+  )
   return (
     <Screen
       scrollEventThrottle={16}
       automaticallyAdjustContentInsets
       style={styles.screen}
     >
-      <View style={styles.view}>
-        <SearchBar
-          placeholder="Search assets ..."
-          containerStyle={{ backgroundColor: 'transparent' }}
-          inputContainerStyle={{ borderRadius: 50 }}
-          //   onChangeText={updateSearch}
-          //   value={search}
+      <View style={{ flex: 1 }}>
+        {renderHeader()}
+        <AssetListScreen
+          navigation={navigation}
+          medias={medias}
+          defaultHeader={() => <View></View>}
+          externalScrollY={scrollY}
         />
       </View>
-      <SearchOptionsList />
-      {/* <AssetList
-        ref={assetListRef}
-        sections={recyclerSections}
-        scrollY={scrollY}
-        onSelectedItemsChange={onSelectedItemsChange}
-        onAssetLoadError={onAssetLoadError}
-        renderFooter={renderFooter}
-        onItemPress={onItemPress}
-        onStoryPress={onStoryPress}
-      /> */}
     </Screen>
   )
 }
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    
   },
 })
