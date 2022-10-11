feccc48700256f4209396254001c1df5765ace28
Mati Q <mahdi.ghorbani@gmail.com>
2022-10-11 20:32:29 +0330

    add some new fields on localDB

--- app/realmdb/realm.ts
+++ app/realmdb/realm.ts
@@ -4,6 +4,6 @@ import { Asset, Box, AssetLocation } from './schemas'
 export const RealmDB = async () =>
   await Realm.open({
     schema: [Asset, Box, AssetLocation],
-    schemaVersion: 6,
+    schemaVersion: 7,
     deleteRealmIfMigrationNeeded: true,
   })
