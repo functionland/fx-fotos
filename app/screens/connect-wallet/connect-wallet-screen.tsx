93a7c33279c6d9b274731e58be771a297386a0d9
Mati Q <mahdi.ghorbani@gmail.com>
2022-11-29 22:25:22 +0330

    Link DID to wallet

--- app/screens/connect-wallet/connect-wallet-screen.tsx
+++ app/screens/connect-wallet/connect-wallet-screen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Alert, StyleProp, StyleSheet, View, ViewStyle } from 'react-native'
-import { Button, Icon, Input, Text } from '@rneui/themed'
+import { Button, FAB, Icon, Image, Text } from '@rneui/themed'
 
 import {
   NativeStackNavigationProp,
@@ -20,9 +20,11 @@ type Props = NativeStackScreenProps<
 type ConnectToWalletStatus = 'None' | 'Connecting' | 'Connected' | 'Failed'
 
 export const ConnectWalletScreen: React.FC<Props> = ({ navigation, route }) => {
-  const [connectToWalletStatus, setConnectToWalletStatus] =
-    useState<ConnectToWalletStatus>('None')
   const walletConnector = useWalletConnect()
+  const [connectToWalletStatus, setConnectToWalletStatus] =
+    useState<ConnectToWalletStatus>(
+      walletConnector.connected ? 'Connected' : 'None',
+    )
   console.log('connectToWalletStatus', connectToWalletStatus)
   useEffect(() => {}, [])
   const renderHeader = () => (
@@ -33,12 +35,14 @@ export const ConnectWalletScreen: React.FC<Props> = ({ navigation, route }) => {
         </Text>
       }
       leftComponent={<HeaderArrowBack navigation={navigation} />}
-      rightComponent={<Icon type="material-community" name="check" />}
     />
   )
   const connectToWallet = async () => {
     try {
-      if (connectToWalletStatus !== 'None') {
+      if (connectToWalletStatus === 'Connected') {
+        await walletConnector.killSession()
+        setConnectToWalletStatus('None')
+      } else if (connectToWalletStatus !== 'None') {
         setConnectToWalletStatus('None')
         return
       }
@@ -65,16 +69,62 @@ export const ConnectWalletScreen: React.FC<Props> = ({ navigation, route }) => {
       style={styles.screen}
     >
       {renderHeader()}
-      <>
+      <View
+        style={{
+          alignItems: 'center',
+        }}
+      >
+        <View style={styles.section}>
+          <Icon name="wallet" type="material-community" size={100} />
+        </View>
+        <View style={styles.section}>
+          <Text style={{ textAlign: 'center' }}>
+            You need to connect the Fx Fotos to a wallet to generate your
+            decentralized ID (DID)
+          </Text>
+        </View>
+        <View style={styles.section}>
+          <Icon name="md-warning-outline" type="ionicon" size={24} />
+          <Text style={{ textAlign: 'center' }}>
+            You should always use the same wallet address or you cannot retrieve
+            your identity and data!
+          </Text>
+        </View>
         <View style={styles.section}>
           <Button
             onPress={connectToWallet}
+            type={connectToWalletStatus === 'Connected' ? 'outline' : 'solid'}
             title={
-              connectToWalletStatus == 'None' ? 'Connect your wallet' : 'Cancel'
+              connectToWalletStatus == 'None'
+                ? 'Connect wallet'
+                : connectToWalletStatus === 'Connecting'
+                ? 'Cancel'
+                : 'Change wallet'
             }
           />
         </View>
-      </>
+        {connectToWalletStatus === 'Connected' && (
+          <>
+            <View style={styles.section}>
+              <Text>{walletConnector?.peerMeta?.name}</Text>
+              <Text style={{ textAlign: 'center' }}>
+                {walletConnector?.accounts?.[0]}
+              </Text>
+            </View>
+            <View style={styles.section}>
+              <Button
+                containerStyle={{
+                  width: 200,
+                }}
+                onPress={() =>
+                  navigation.navigate(AppNavigationNames.CreateDIDScreen)
+                }
+                title="Next step"
+              />
+            </View>
+          </>
+        )}
+      </View>
     </Screen>
   )
 }
@@ -84,7 +134,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   section: {
-    paddingTop: 20,
+    paddingHorizontal: 30,
+    paddingTop: 30,
     alignItems: 'center',
+    width: '100%',
   },
 })
